bdee21a336450a48902486f77e2667130078fba5
bfrodo <valiumz@gmail.com>
2015-03-23 23:31:36 -0400

    c

---
+++ app/src/main/java/one/valiumz/firstapp/Splash.java
@@ -0,0 +1,14 @@
+package one.valiumz.firstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Splash extends Activity {
+
+    protected void onCreate(Bundle Splashscreen) {
+        super.onCreate(Splashscreen);
+        setContentView(R.layout.splash);
+
+    }
+}
+
