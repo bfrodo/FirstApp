9ccac114e90ab541439e6b645ca1784ec27a4356
bfrodo <valiumz@gmail.com>
2015-03-23 23:49:36 -0400

    c

--- app/src/main/java/one/valiumz/firstapp/Splash.java
+++ app/src/main/java/one/valiumz/firstapp/Splash.java
@@ -1,6 +1,7 @@
 package one.valiumz.firstapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends Activity {
@@ -8,7 +9,19 @@ public class Splash extends Activity {
     protected void onCreate(Bundle Splashscreen) {
         super.onCreate(Splashscreen);
         setContentView(R.layout.splash);
-
-    }
-}
+        Thread t = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e){
+                        e.printStackTrace();
+                }finally {
+                    Intent openStartingPoint = new Intent("one.valiumz.firstapp.STARTINGPOINT");
+                    startActivity(openStartingPoint);
+                }
+                }
+            };
+            t.start();
+        }
+        }
 
